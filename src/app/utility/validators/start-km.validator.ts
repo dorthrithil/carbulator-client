b4bb9dd174e93b4a6a1a32d68f9278db9d88fe35
Felix Engelmann <f.engelmann@wizai.com>
2018-10-28 11:36:52 +0100

    Fix tour km validation documentation

--- src/app/utility/validators/start-km.validator.ts
+++ src/app/utility/validators/start-km.validator.ts
@@ -2,7 +2,7 @@ import {AbstractControl, ValidatorFn} from '@angular/forms';
 import {toNumber} from '../conversion/to-number';
 
 /**
- * Validator for start kilometer. Only valid if equal to last end km.
+ * Validator for start kilometer. Only valid if equal or higher then last end km.
  * @return Validator function for start km.
  */
 export function startKmValidator(lastEndKm: number): ValidatorFn {
