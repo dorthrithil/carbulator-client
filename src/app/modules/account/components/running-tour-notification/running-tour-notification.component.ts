a6a8aaa9a2b7c1cc386d31408ac03a506e48ebe8
Felix Engelmann <f.engelmann@wizai.com>
2019-02-06 14:23:15 +0100

    Add cancel tour button to running tour notification

--- src/app/modules/account/components/running-tour-notification/running-tour-notification.component.ts
+++ src/app/modules/account/components/running-tour-notification/running-tour-notification.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Notification} from '../../../../models/notification';
 import {NavNotificationsService} from '../../../../services/core/nav-notifications.service';
+import {Observable} from 'rxjs';
+import {MessageResponse} from '../../../../services/crud/auth-crud.service';
+import {TourService} from '../../../../services/crud/tour.service';
 
 /**
  * A component for showing a notification when a tour is running.
@@ -10,22 +13,40 @@ import {NavNotificationsService} from '../../../../services/core/nav-notificatio
   templateUrl: './running-tour-notification.component.html',
   styleUrls: ['./running-tour-notification.component.scss']
 })
-export class RunningTourNotificationComponent {
+export class RunningTourNotificationComponent implements OnInit {
 
   /**
    * The running tour notification of this component.
    */
   @Input() runningTour: Notification;
 
-  constructor(private navNotifications: NavNotificationsService) {
+  public cancelTourModalOpen = false;
+  public deleteTourRequest: Observable<MessageResponse>;
+
+  constructor(private navNotifications: NavNotificationsService,
+              private tourService: TourService) {
+  }
+
+  /**
+   * Sets up the delete tour request.
+   */
+  ngOnInit(): void {
+    this.deleteTourRequest = this.tourService.deleteTour(this.runningTour.subject);
   }
 
   /**
    * Marks the notification as closed.
    */
-  public onTourFinished() {
+  public onTourFinishedOrCanceled() {
     this.runningTour.isOpen = false;
     this.navNotifications.decrementNotificationsCount();
+  }
+
+  /**
+   * Opens the cancel tour modal.
+   */
+  public openCancelTourModal() {
+    this.cancelTourModalOpen = true;
   }
 
 }
